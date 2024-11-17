4eccc6e31429261c1cec1e4e1e24a6e5bbdfb408
spicymanp <info@paulotoureiro.com>
2024-11-17 18:39:02 +0200

    changed dir

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 // connect to html elements here 
 const playerName = ducment.getElementById("player-name");
-
+const dropdown = document.getElementById("player-selection-dropdown")
+const displayPlayerSelection = document.getElementById("current-selection");
 // rest of my code 
+
+
+const changeStuff = () => {
+    const selectedMove = dropdown.options[dropdown.selectedIndex].text;
+    console.log(selectedMove);
+    displayPlayerSelection.textContent = selectedMove;
+}
+
+dropdown.addEventListener('change', function() {
+    console.log("heeeeeeeeeeeeee");
+    changeStuff();
+})
+
 
 // functions and functionality needed 
 // set player name
