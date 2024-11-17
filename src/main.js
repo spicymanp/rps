da8d43957e18d662b1b615b34a47b60ff0f69090
spicymanp <info@paulotoureiro.com>
2024-11-17 14:21:48 +0200

    removing tailwindcss for now, focussing on game logic

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-// where the magic of rps will happen I believe.
+// connect to html elements here 
+const playerName = ducment.getElementById("player-name");
+
+// rest of my code 
+
+// functions and functionality needed 
+// set player name
+// get / set player scores (including computer player(cp)
+// game logic - random rolls for cp and comparing results between player selection and computers selection.
+// update html elements using even listeners
+
